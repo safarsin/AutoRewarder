16bd1284fdec77c0f3bb107ab3705c32e358b5a9
Sino <sinosafarov1919@gmail.com>
2026-04-15 13:03:27 +0200

    Feat: add new css file settings.css

--- GUI/settings.js
+++ GUI/settings.js
@@ -1,14 +1,24 @@
-// Settings section handler
-const settingsBtn = document.getElementById('settingsBtn');
-const closeSettingsBtn = document.getElementById('closeSettingsBtn');
-const settingsPanel = document.getElementById('settingsPanel');
+document.addEventListener('DOMContentLoaded', () => {
 
-// Open settings panel
-settingsBtn.addEventListener('click', () => {
+  // Settings section handler
+  const settingsBtn = document.getElementById('settingsBtn');
+  const closeSettingsBtn = document.getElementById('closeSettingsBtn');
+  const settingsPanel = document.getElementById('settingsPanel');
+
+  // Open settings panel
+  settingsBtn.addEventListener('click', () => {
     settingsPanel.classList.toggle('open');
-});
+  });
 
-// Close settings panel
-closeSettingsBtn.addEventListener('click', () => {
+  // Close settings panel
+  closeSettingsBtn.addEventListener('click', () => {
     settingsPanel.classList.remove('open');
+  });
+
+  // Settings elements
+  const autoStartUp = document.getElementById('autoStartUp');
+  const advancedScheduling = document.getElementById('advancedScheduling');
+  const advSchedulingWrapper = document.getElementById('advSchedulingWrapper');
+  const runDurationWrapper = document.getElementById('runDurationWrapper');
+  const saveSettingsBtn = document.getElementById('saveSettingsBtn');
 });
